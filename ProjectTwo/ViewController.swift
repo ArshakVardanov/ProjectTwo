ca3a6fefc914e3396c71a53ff073509f4e789a6b
ArshakVardanov <103132731+ArshakVardanov@users.noreply.github.com>
2022-05-16 20:02:16 +0400

    Some changes

--- ProjectTwo/ViewController.swift
+++ ProjectTwo/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Some changes
         // Do any additional setup after loading the view.
     }
 
