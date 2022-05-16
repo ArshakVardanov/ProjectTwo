eec1dc47d1d6ed38e6a5ec02e682c4d41b88fc93
ArshakVardanov <103132731+ArshakVardanov@users.noreply.github.com>
2022-05-16 20:00:24 +0400

    Inital commit

---
+++ ProjectTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectTwo
+//
+//  Created by Arshak Vardanov on 16.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
